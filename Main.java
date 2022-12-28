1e2442952a08cd2169e3a37268e51ef335aacaaf
george.almousa <george.almousa@progressoft.com>
2022-12-28 21:19:25 +0300

    chore: adding the source code

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
 //        Person1 person1 = new Person1();
--- README.md
+++
@@ -1,2 +0,0 @@
-# data-structure
-data-structure including LinkedList, ArrayList, Stack, Queue and Hash Table
