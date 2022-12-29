eeda09dbb212fe698efd297247392c78d501087a
george.almousa <george.almousa@progressoft.com>
2022-12-29 13:56:03 +0300

    "chore: #3 methods explained"

--- Main.java
+++ Main.java
@@ -1,30 +1,21 @@
 import java.util.ArrayList;
 public class Main {
+    public static int x; // this is a global variable outside the class
     public static void main(String[] args) {
-        Box<String> stringBox= new Box<>();
-        stringBox.setItem("Yellow box");
-        System.out.println(stringBox.getItem());
-        Box <Integer> integerBox=new Box<>();
-        integerBox.setItem(1);
-        System.out.println(integerBox.getItem()); //1
-
-        ArrayList<Integer> myNum= new ArrayList<>(10);
-        for(int i=0; i<10;i++){
-            myNum.add(i);
-        }
-
-        System.out.println(getMiddleArrayListElement(myNum));
-
-        String [] myCars={"BMW","MAZDA","KIA","FORD","TOYOTA","NISSAN"};
-
-        System.out.println(Main.<String>getMiddleEelementOfArray(myCars));
-
+        System.out.println(calcRecArea(10,10));
+        x = 10;
+        System.out.println(x);
     }
-    public static <T> T getMiddleArrayListElement(ArrayList<T> array){
-        return array.get(array.size() / 2);
+    public static int calcRecArea(int length, int width){
+        int area = length*width;
+        x = 100; // this is a global variable;
+        return area;
     }
-
-    public static <T> T getMiddleEelementOfArray(T [] array){
-        return array[array.length/2];
+    // this is the method overloading
+    static int calcRecArea(int sideLength){
+        int area= sideLength/2;
+        // area is a local variable in java scope
+        return area;
     }
 }
+
---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        // as you see we can access the variable name above.
+    }
+
+    String name; // this is called an instance variable which can be accessible form same class only
+
+}
